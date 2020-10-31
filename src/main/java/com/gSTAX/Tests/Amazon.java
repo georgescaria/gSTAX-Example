7af31a8bbf5a8b7da41c2918751de240e1e4ed7d
gscaria1 <sdygeorgescaria@gmail.com>
2020-10-31 20:39:10 +0530

    Update

--- src/main/java/com/gSTAX/Tests/Amazon.java
+++ src/main/java/com/gSTAX/Tests/Amazon.java
@@ -1,15 +1,10 @@
 package com.gSTAX.Tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.aventstack.extentreports.Status;
 import com.gSTAX.Setup.InitialSetup;
-import com.gSTAX.TestData.Excel_Data;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.gSTAX.TestData.ExcelData;
+import com.gSTAX.Utilities.Report;
 
 public class Amazon extends InitialSetup 
 {
@@ -19,7 +14,8 @@ public class Amazon extends InitialSetup
     {
         driver.get("https://amazon.in");
         String title = driver.getTitle();
-        Report.log(Status.PASS,title);
+        test.log(Status.PASS,title);
+        Report.takeScreenshot();
     }
 }
 
